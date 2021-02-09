790f81ab137db2358a81a54f205ef5074e999e3f
chaitanya1911 <77531364+chaitanya1911@users.noreply.github.com>
2021-02-09 16:11:48 +0530

    Update brain.dart

--- lib/brain.dart
+++ lib/brain.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Brain {
   final serverKey =
-      "AAAAPyRoId0:APA91bHOcs1fUJ-BOHwMWI_13G8CTyFtlYC5JjQWN2vmZC1anZuwyTUX_KLQzkYWnvKR_LxXx8m-W6QWjTP8U-uMt0acNgKoaB0avTBSKaIVfhYYCp0l9_r6mLqIyUr_psaRI2gTqi4I";
+      "";
   getUser(String email) {
     return FirebaseFirestore.instance.collection("users").doc(email);
   }
